b5dea97abbbb55117d565a34e75835ff081e5e97
RoyNisimov <roynisimov@gmail.com>
2023-11-25 15:30:23 +0200

    Updated to cha-hashing@0.4.8.1

--- my_cryptography/OAEP.py
+++ my_cryptography/OAEP.py
@@ -17,17 +17,26 @@ class OAEP:
         return bytes(encoded)
 
     @staticmethod
-    def oaep_pad(message, nonce):
+    def oaep_pad(message):
+        nonce = secrets.randbits(32)
+        nonce = nonce.to_bytes(32, sys.byteorder)
         mm = message + b"\x00" * (32-len(message))
         G = OAEP.repeated_key_xor(mm, hashlib.sha256(nonce).digest())
         H = OAEP.repeated_key_xor(nonce, hashlib.sha256(G).digest())
         return G + H
 
     @staticmethod
+    def oaep_unpad(message):
+        oaep_step2 = message + b'\x00' * (64 - len(message))
+        G = oaep_step2[:32]
+        H = oaep_step2[32:64]
+        nonce = OAEP.repeated_key_xor(H, hashlib.sha256(G).digest())[:32]
+        mm = OAEP.repeated_key_xor(G, hashlib.sha256(nonce).digest())
+        return mm
+
+    @staticmethod
     def encrypt(msg, n, pub):
-        nonce = secrets.randbits(32)
-        nonce = nonce.to_bytes(32, sys.byteorder)
-        oaep = OAEP.oaep_pad(msg, nonce)
+        oaep = OAEP.oaep_pad(msg)
         m_int = int.from_bytes(oaep, sys.byteorder)
         ret_int = pow(m_int, pub, n)
         ret_b = ret_int.to_bytes(ret_int.bit_length(), sys.byteorder)
@@ -37,19 +46,11 @@ class OAEP:
     def decrypt(ciphertext, n, priv):
         rsa_int = int.from_bytes(ciphertext, sys.byteorder)
         oaep_step1 = pow(rsa_int, priv, n)
-        oaep_step2 = oaep_step1.to_bytes(oaep_step1.bit_length(), sys.byteorder)
-        oaep_step2 = oaep_step2 + b'\x00' * (32 - len(oaep_step2))
-        G = oaep_step2[:32]
-        H = oaep_step2[32:64]
-        nonce = OAEP.repeated_key_xor(H, hashlib.sha256(G).digest())[:32]
-        mm = OAEP.repeated_key_xor(G, hashlib.sha256(nonce).digest())
-        return mm
+        return OAEP.oaep_unpad(oaep_step1)
 
     @staticmethod
     def encrypt_ElGamal(key: ElGamal.ElGamalKey, msg):
-        nonce = secrets.randbits(32)
-        nonce = nonce.to_bytes(32, sys.byteorder)
-        oaep = OAEP.oaep_pad(msg, nonce)
+        oaep = OAEP.oaep_pad(msg)
         oaep_int = int.from_bytes(oaep, sys.byteorder)
         if oaep_int >= key.p: OAEP.encrypt_ElGamal(key, msg)
         c1, c2 = ElGamal.ElGamal.encrypt(key, oaep)
@@ -58,18 +59,11 @@ class OAEP:
     @staticmethod
     def decrypt_ElGamal(key, c1, c2):
         oaep = ElGamal.ElGamal.decrypt(key, c1, c2)
-        oaep_step2 = oaep + b'\x00' * (64 - len(oaep))
-        G = oaep_step2[:32]
-        H = oaep_step2[32:64]
-        nonce = OAEP.repeated_key_xor(H, hashlib.sha256(G).digest())[:32]
-        mm = OAEP.repeated_key_xor(G, hashlib.sha256(nonce).digest())
-        return mm
+        return OAEP.oaep_unpad(oaep)
 
     @staticmethod
     def encrypt_BlackFrog(key: BlackFrogKey, msg: bytes):
-        nonce = secrets.randbits(32)
-        nonce = nonce.to_bytes(32, sys.byteorder)
-        oaep = OAEP.oaep_pad(msg, nonce)
+        oaep = OAEP.oaep_pad(msg)
         oaep_int = int.from_bytes(oaep, sys.byteorder)
         if oaep_int >= key.n: OAEP.encrypt_BlackFrog(key, msg)
         cipher = BlackFrog.encrypt(key, oaep)
@@ -78,12 +72,7 @@ class OAEP:
     @staticmethod
     def decrypt_BlackFrog(key: BlackFrogKey, cipher: bytes):
         oaep = BlackFrog.decrypt(key, cipher)
-        oaep_step2 = oaep + b'\x00' * (64 - len(oaep))
-        G = oaep_step2[:32]
-        H = oaep_step2[32:64]
-        nonce = OAEP.repeated_key_xor(H, hashlib.sha256(G).digest())[:32]
-        mm = OAEP.repeated_key_xor(G, hashlib.sha256(nonce).digest())
-        return mm
+        return OAEP.oaep_unpad(oaep)
 
 if __name__ == '__main__':
     pub, priv = ElGamal.ElGamal.generate_keys(1024)
